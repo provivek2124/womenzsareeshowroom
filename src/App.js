3a97b375731b7e84749cb0967a3141b9867951a8
vivek kumar <provivek2124@gmail.com>
2020-04-29 00:14:30 +0530

    Updated menu items with Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom';
 import HomePage from './Pages/homepage/homepage.component';
+
+const HatsPage = () =>(
+  <div>
+    <h1>hats page</h1>
+  </div>
+)
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={HatsPage}/>
     </div>
   );
 }
